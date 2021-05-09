d745b98aa50e4c6695255866bf19a7f2cdfdc22c
Umangi567 <81431350+Umangi567@users.noreply.github.com>
2021-05-09 17:13:17 +0530

    Update Add.java
    add override comments

--- Add.java
+++ Add.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Add extends AppCompatActivity {
-
+    //override 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
