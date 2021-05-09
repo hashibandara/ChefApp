3073d281181731f941fe5a85f38376b454a40be8
Umangi567 <81431350+Umangi567@users.noreply.github.com>
2021-05-09 17:14:54 +0530

    Update FoodData.java

--- FoodData.java
+++ FoodData.java
@@ -11,41 +11,4 @@ public class FoodData {
         this.itemDescription = itemDescription;
         this.itemPrice = itemPrice;
         this.itemImage = itemImage;
-    }
-
-    public String getItemName() {
-        return itemName;
-    }
-
-    public void setItemName(String itemName) {
-        this.itemName = itemName;
-    }
-
-    public String getItemDescription() {
-        return itemDescription;
-    }
-
-    public void setItemDescription(String itemDescription) {
-        this.itemDescription = itemDescription;
-    }
-
-    
-    public String getItemPrice() {
-        return itemPrice;
-    }
-
-    
-    public void setItemPrice(String itemPrice) {
-        this.itemPrice = itemPrice;
-    }
-
-    
-    public String getItemImage() {
-        return itemImage;
-    }
-
-    
-    public void setItemImage(String itemImage) {
-        this.itemImage = itemImage;
-    }
-}
+  
